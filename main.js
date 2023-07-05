db93f4f47be407799217e3017f54b3615651a9f8
SnakyMz <Soban.syeed@outlook.com>
2023-07-06 00:30:54 +0530

    Add local Storage

--- main.js
+++ main.js
@@ -1,20 +1,13 @@
 const booksListContainer = document.querySelector('#booksListContainer');
-const addButton = document.querySelector('#addButton');
-const Title = document.querySelector('#Title');
-const Author = document.querySelector('#Author');
+const form = document.querySelector('form');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
 
-let books = [
-    {
-        title: 'FirstBook',
-        author: 'TestyTesty',
-    },
-    {
-        title: 'SecondBook',
-        author: 'TestyTesty',
-    }
-];
+let books = [];
 
 function generateBooks() {
+    booksListContainer.innerHTML = '';
+    books = JSON.parse(localStorage.getItem("Library"));
     books.forEach((book) => {
         const bookDiv = document.createElement('div');
         bookDiv.className = 'bookDiv';
@@ -28,6 +21,7 @@ function generateBooks() {
         removeButton.className = 'removeButton';
         removeButton.type = 'button';
         removeButton.innerHTML = 'Remove';
+        removeButton.id = `${book.id}`;
         const hr = document.createElement('hr');
         bookDiv.appendChild(bookTitle);
         bookDiv.appendChild(bookAuthor);
@@ -35,11 +29,35 @@ function generateBooks() {
         bookDiv.appendChild(hr);
         booksListContainer.appendChild(bookDiv);
         removeButton.addEventListener('click', () => {
-            booksListContainer.removeChild(bookDiv);
+            removeBook(`${book.id}`);
         });
     });
 };
 
-addButton.addEventListener('click', () => {
+function removeBook(id) {
+    books = books.filter(book => book.id !== id);
+    localStorage.setItem('Library', JSON.stringify(books));
     generateBooks();
+}
+
+function addBook(title, author) {
+    let bookObject = {};
+    bookObject.title = title;
+    bookObject.author = author;
+    bookObject.id = title + author;
+    books.push(bookObject);
+    localStorage.setItem('Library', JSON.stringify(books));
+    generateBooks();
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addBook(title.value, author.value);
+    form.reset();
+});
+
+window.addEventListener("load", () => {
+    if (localStorage.getItem("Library")) {
+        generateBooks();
+    }
 });
